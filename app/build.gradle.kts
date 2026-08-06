d5be43677d608251371cd725380d07db320ea217
Dcsgo <dcsgomail@163.com>
2026-08-06 15:00:20 +0800

    v3.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "cn.com.dcsgo.mihx"
         minSdk = 33
         targetSdk = 36
-        versionCode = 18
-        versionName = "3.0.0"
+        versionCode = 19
+        versionName = "3.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
